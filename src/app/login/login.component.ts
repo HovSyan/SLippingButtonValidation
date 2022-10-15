5f8696dc5beb26c2ceead35bac29dfc4d2340821
Hovhannes Sahakyan <syanhovhannes@gmail.com>
2022-10-15 14:48:46 +0400

    feat: add animatation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,20 +1,33 @@
-import { Component } from '@angular/core';
-import {FormControl, Validators} from "@angular/forms";
+import {Component, ViewChild} from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  animations: [
+    trigger('slip', [
+      state('left', style({textAlign: 'left'})),
+      state('center', style({textAlign: 'center'})),
+      state('right', style({textAlign: 'right'})),
+      transition('* => *', [
+        animate('.1s')
+      ])
+    ])
+  ]
 })
 export class LoginComponent {
+  @ViewChild('loginButton') loginButton!: MatButton;
+  loginButtonAnimationState = 'left';
   username: string = '';
-  usernameFormControl = new FormControl('', Validators.required);
   password: string = '';
   showInvalidFields: boolean = false;
 
   onMouseEnter() {
     if(!this.isFormValid()) {
       this.showInvalidFields = true;
+      this.slipButton();
     }
   }
 
@@ -28,5 +41,11 @@ export class LoginComponent {
 
   isPasswordValid() {
     return this.password != '';
+  }
+
+  private slipButton() {
+    const states = ['left', 'center', 'right', 'left'];
+    this.loginButtonAnimationState = states[states.indexOf(this.loginButtonAnimationState) + 1];
+    console.log(this.loginButtonAnimationState);
   }
 }
